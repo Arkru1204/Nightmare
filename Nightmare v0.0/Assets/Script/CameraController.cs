a3f48eaac22e9c6f034f2084c44336dfa38f32d9
Arkru <parkkr1204@naver.com>
2022-11-07 01:34:50 +0900

    v0.0
    움직임, 물리 구현

---
+++ Nightmare v0.0/Assets/Script/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraXpos = 3;
+    public float cameraYpos = 2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = player.transform.position;  // player의 위치값
+        transform.position = new Vector3(playerPos.x + cameraXpos, playerPos.y + cameraYpos, transform.position.z);
+        // x,z값은 카메라의 x,z값 y값은 playerPos의 y값으로 가져옴
+    }
+}
