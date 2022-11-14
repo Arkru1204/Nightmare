b510eaed2ae5e9913e48d7886680cd9e99ec667d
Arkru <parkkr1204@naver.com>
2022-11-15 03:13:30 +0900

    v0.018
    공격 모션에 광선 추가

---
+++ Nightmare v0.0/Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
