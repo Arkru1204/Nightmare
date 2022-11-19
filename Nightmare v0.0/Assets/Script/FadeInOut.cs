05b75bd04e125bb6b3153a349b11fba91045b79d
Arkru <parkkr1204@naver.com>
2022-11-20 04:08:42 +0900

    v0.026
    재시작 버튼 추가
    재시작 페이드 인 추가

---
+++ Nightmare v0.0/Assets/Script/FadeInOut.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInOut : MonoBehaviour
+{
+    public bool fadeIn = true;
+    public float fadeTime = 1f;
+
+    Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+
+        image.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    private void Start()
+    {
+        if (fadeIn)
+            StartCoroutine(FadeIn(fadeTime));
+        else
+            StartCoroutine(FadeOut(fadeTime));
+    }
+
+    public IEnumerator FadeIn(float time)
+    {
+        Color color = image.color;
+        while (color.a < 1f)
+        {
+            color.a += Time.deltaTime / time;
+            image.color = color;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOut(float time)
+    {
+        Color color = image.color;
+        while (color.a > 0f)
+        {
+            color.a -= Time.deltaTime / time;
+            image.color = color;
+            yield return null;
+        }
+    }
+}
--- Nightmare v0.0/Assets/Script/GameManager.cs
+++ Nightmare v0.0/Assets/Script/GameManager.cs
@@ -1,34 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public PlayerMain player;
     public int hp = 3;
-    public GameObject[] HpUI;
-    public Animator[] HpAnim;
+
+    public GameObject[] hpUI;
+    public Animator[] hpAnim;
+    public GameObject restartButton;
 
     public void HpDown()
     {
         hp--;
-        if (hp == 0)
+        if (hp <= 0)
             StartCoroutine(Dead());
 
-        HpAnim[hp].SetBool("isHpDestroy", true);
+        hpAnim[hp].SetBool("isHpDestroy", true);
         StartCoroutine(DestroyHp(hp)); // 매개변수 전달을 위해서 코루틴 사용
     }
 
     IEnumerator DestroyHp(int i)
     {
         yield return new WaitForSeconds(0.7f); // 0.8초 뒤 독립시행
-        HpUI[i].SetActive(false);
+        hpUI[i].SetActive(false);
     }
 
     IEnumerator Dead()
     {
         yield return new WaitForSeconds(0.35f);
         Time.timeScale = 0;
+
+        restartButton.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
