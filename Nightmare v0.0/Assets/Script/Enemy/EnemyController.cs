ce4481a7526c867f6a62b4c7fe46dc61fbf1eed1
Arkru <parkkr1204@naver.com>
2022-12-04 08:18:18 +0900

    v0.038
    보스 공격 추가
    inAreaFire 방향 바라보고 발사 추가

--- Nightmare v0.0/Assets/Script/Enemy/EnemyController.cs
+++ Nightmare v0.0/Assets/Script/Enemy/EnemyController.cs
@@ -7,6 +7,9 @@ public class EnemyController : MonoBehaviour
     public float maxSpeed = 2.5f;
     public float raycastDistance = 1f;
 
+    // 인스팩터X
+    [System.NonSerialized] public int saveDir = 1;
+
     int nextMove; // 행동 지표를 결정할 변수 하나 생성
 
     Rigidbody2D rigid;
@@ -31,9 +34,9 @@ public class EnemyController : MonoBehaviour
             rigid.velocity = new Vector2(nextMove * maxSpeed, rigid.velocity.y);
 
             //지형 체크
-            Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.4f, rigid.position.y);
+            Vector2 frontVec = new Vector2(rigid.position.x + nextMove * raycastDistance, rigid.position.y);
 
-            //Debug.DrawRay(frontVec, Vector3.down, new Color(0, 2f, 0)); // 에디터 상에서만 레이를 그려준다
+            //Debug.DrawRay(frontVec, Vector3.down, new Color(0, raycastDistance, 0)); // 에디터 상에서만 레이를 그려준다
             RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, raycastDistance, LayerMask.GetMask("Map"));
             if (rayHit.collider == null) // 바닥 감지가 없을 때
                 Turn();
@@ -54,10 +57,16 @@ public class EnemyController : MonoBehaviour
     void Move() // 이동 방향 결정
     {
         nextMove = Random.Range(-1, 2); // -1, 0, 1
+
+        if (nextMove == 1)  // 방향 저장용
+            saveDir = 1;
+        else if (nextMove == -1)
+            saveDir = -1;
+
         Animate();
     }
 
-    void Turn() // 속도는 유지한채로 방향만 전환 -> 다음 액션까지 2초 연장
+    public void Turn() // 속도는 유지한채로 방향만 전환 -> 다음 액션까지 2초 연장
     {
         nextMove *= -1;
         Animate();
@@ -71,6 +80,22 @@ public class EnemyController : MonoBehaviour
         anim.SetInteger("runSpeed", nextMove);
 
         if (nextMove != 0)
-            spriteRenderer.flipX = nextMove == 1;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -nextMove, transform.localScale.y, transform.localScale.z);
+        //spriteRenderer.flipX = nextMove == 1;
+    }
+
+    public void lookBack()
+    {   
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    public int getThink()
+    {
+        return nextMove;
+    }
+
+    public int getDir()
+    {
+        return saveDir;
     }
 }
