8705de855acb84ff24982ea43e1125b0716107f8
Kim-Yundong-KYD <kimyd123456@gmail.com>
2022-12-03 14:32:39 +0900

    v0.034b
    구름 이미지 변경, blade Trap 추가, 몹 2마리 추가

---
+++ Nightmare v0.0/Assets/Script/Blade_MoveH.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade_MoveH : MonoBehaviour
+{
+    public float speed = 1;
+    public int maxCount = 100;
+    int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        count += 1;
+
+        if (count >= maxCount)
+        {
+            count = 0;
+            speed *= -1;
+        }
+
+        this.transform.Translate(speed / 50, 0, 0);
+    }
+}
