8aaf2b5e37841d2690e051e91e6e7d590e21cbd3
Arkru <parkkr1204@naver.com>
2022-12-05 01:46:09 +0900

    v0.041
    BossA 일반공격 추가

---
+++ Nightmare v0.0/Assets/Script/Enemy/InArea.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InArea : MonoBehaviour
+{
+    bool isArea = false;
+    int playerDir = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isArea = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") // 플레이어 위치가 변경되면 방향 확인
+        {
+            playerDir = transform.position.x - collision.transform.position.x > 0 ? -1 : 1; // -1 왼쪽, 1 오른쪽
+            isArea = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") // 플레이어가 나가면 발사 중지
+        {
+            isArea = false;
+        }
+    }
+
+    public bool getIsArea()
+    {
+        return isArea;
+    }
+
+    public int getPlayerDir()
+    {
+        return playerDir;
+    }
+}
